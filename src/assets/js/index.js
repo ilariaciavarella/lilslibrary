a3801eae2496927f962c9cfecc42225528157dd6
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2024-01-17 19:15:35 +0100

    Move project to new folder to initialise it with webpack

---
+++ src/assets/js/index.js
@@ -0,0 +1,5 @@
+import '../scss/styles.scss';
+import '../scss/resultsPage.scss';
+
+import ArrowIcon from '../imgs/icons/submit_arrow.svg';
+document.querySelector('img').setAttribute('src', ArrowIcon);
