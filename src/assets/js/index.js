46033c682570ad0c8df449959cdee571d2df08fd
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2024-01-18 19:09:18 +0100

    Start working on interaction from button in homepage

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,4 @@
+// IMPORT
 import axios from 'axios';
 import _ from 'lodash';
 
@@ -5,3 +6,19 @@ import '../scss/styles.scss';
 
 import ArrowIcon from '../imgs/icons/submit_arrow.svg';
 document.querySelector('img').setAttribute('src', ArrowIcon);
+
+// SCRIPT
+const submitBtn = document.querySelector('#submit-btn');
+const searchInput = document.querySelector('#search-input');
+
+const getUrl = function (genre) {
+    return `${process.env.OPEN_LIBRARY}${genre}.json`;
+}
+
+let fetchUrl;
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetchUrl = getUrl(searchInput.value);
+    // window.open('results.html', '_self');
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
@@ -42,7 +43,8 @@ const config = {
         }),
 
         new MiniCssExtractPlugin(),
-        new LodashModuleReplacementPlugin()
+        new LodashModuleReplacementPlugin(),
+        new Dotenv(),
 
     ],
     module: {
