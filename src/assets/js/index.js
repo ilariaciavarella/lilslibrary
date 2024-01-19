1b0ab93d0c372654951d5f2eedc97e067b8b62de
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2024-01-19 18:45:59 +0100

    Start writing script to fetch data

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,11 +1,25 @@
 // IMPORT
-import axios from 'axios';
-import _ from 'lodash';
-
 import '../scss/styles.scss';
 
 import ArrowIcon from '../imgs/icons/submit_arrow.svg';
 document.querySelector('img').setAttribute('src', ArrowIcon);
 
 // SCRIPT
-// 
+const submitBtn = document.querySelector('#submit-btn');
+const searchInput = document.querySelector('#search-input');
+const bookCollection = document.querySelector('.collection');
+
+function getUrl(userInput) {
+    if (userInput.match(/^[a-zA-Z\s]*$/gi)) {
+        return `${process.env.OPEN_LIBRARY}${userInput.toLowerCase().split(' ').join('_')}.json`;
+    } else {
+        userInput = userInput.replace(/[_\d\W]/gi, "");
+        return `${process.env.OPEN_LIBRARY}${userInput.toLowerCase().split(' ').join('_')}.json`;
+    }
+}
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem("chosenGenre", getUrl(searchInput.value));
+    window.open('results.html', '_self');
+})
--- src/assets/js/results.js
+++ src/assets/js/results.js
@@ -1,1 +1,29 @@
+// IMPORT
+import axios from 'axios';
+import _ from 'lodash';
+
 import '../scss/results.scss';
+
+// SCRIPTS
+function getBooks(fetchUrl) {
+    console.log(fetchUrl);
+    axios.get(fetchUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            // data.works.forEach(work => {
+            //     const book = bookTemplate.content.cloneNode('true').children[0];
+            //     const title = book.querySelector('.title');
+            //     const author = book.querySelector('.author');
+
+
+            //     title.textContent = work.title;
+
+            //     for (authorNames of work.authors) {
+            //         author.textContent += authorNames.name
+            //     }
+
+            //     bookCollection.append(book);
+            // })
+        })
+}
